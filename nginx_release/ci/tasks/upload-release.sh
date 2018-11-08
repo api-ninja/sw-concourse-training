fc55e536a1f341dda1f8b2982b85b26004090836
Travis Lawall <travis.lawall@digitalglobe.com>
2018-11-08 22:33:52 +0000

    correcting flag

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -9,7 +9,7 @@ export BOSH_ENVIRONMENT=training
 
 export CF_TRACE=true
 
-bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR}
+bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert ${CA_CERT_URL} -e ${BOSH_DIRECTOR}
 
 cd source-code/nginx_release
 
