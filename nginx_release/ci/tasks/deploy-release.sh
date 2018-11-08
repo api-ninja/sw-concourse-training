49fecc0fac6b563245bdf78a95a889b4d709eba0
Travis Lawall <travis.lawall@digitalglobe.com>
2018-11-08 23:07:54 +0000

    more changes

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -5,7 +5,7 @@
 export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
-export BOSH_DEPLOYMENT=ninja-release
+export BOSH_DEPLOYMENT=api-ninja-release
 export BOSH_DIRECTOR=https://10.4.1.4:25555
 export BOSH_ENVIRONMENT=training
 
