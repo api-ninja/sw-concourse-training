70b43c74c3759a1dfbf57d772e2e7e6236989307
Travis Lawall <travis.lawall@digitalglobe.com>
2018-11-08 22:18:24 +0000

    modifying upload-release.sh to upload.

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -6,3 +6,9 @@ export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_DEPLOYMENT=ninja-release
 export BOSH_DIRECTOR=https://10.4.1.4:25555
 export BOSH_ENVIRONMENT=training
+
+
+cd source-code/nginx_release
+
+#bosh upload-release releases/release.gz -d ninja-release
+bosh upload-release releases/release.gz -d ninja-release
