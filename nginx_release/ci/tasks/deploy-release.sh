aa963975fb5e1ccad2c657e9a8f50381a82dde2d
Travis Lawall <travis.lawall@digitalglobe.com>
2018-11-08 21:51:51 +0000

    adding deploy release script

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-export CA_CERT_URL=https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-export BOSH_DEPLOYMENT=<replace-me>-nginx
-export BOSH_DIRECTOR=https://10.200.192.0:25555/
+#!/usr/bin/env bash
+# These variables are provided by your instructor
+export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=<replace-me>
-export BOSH_ENVIRONMENT=training-bosh
+export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
+export BOSH_DEPLOYMENT=nginx-release
+export BOSH_DIRECTOR=https://10.4.1.4:25555
+export BOSH_ENVIRONMENT=training
+
 
 cd source-code/nginx_release
 
